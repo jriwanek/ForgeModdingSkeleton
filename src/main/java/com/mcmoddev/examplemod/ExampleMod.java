52d4645fb6d32f946412ed57066e9f7597279e77
Jasmine Iwanek <jasmine@iwanek.co.uk>
2018-07-23 01:30:28 -0700

    Add example of pushing version from script to mod

--- src/main/java/com/mcmoddev/examplemod/ExampleMod.java
+++ src/main/java/com/mcmoddev/examplemod/ExampleMod.java
@@ -44,6 +44,7 @@ public final class ExampleMod {
 
 	/** Version of this Mod. */
 	public static final String VERSION = "1.0.0";
+//	public static final String VERSION = "@MOD_VERSION@"
 
     @SidedProxy(
 				clientSide = "com.mcmoddev.examplemod.proxy.ClientProxy",
