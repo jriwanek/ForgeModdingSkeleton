f6b0bd7bd6560e14ba8ef2c5f98e724f138f2338
Jasmine Iwanek <jasmine@iwanek.co.uk>
2019-10-31 00:41:55 -0700

    Actually use 1.7.10 code

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,20 +1,19 @@
 package com.example.examplemod;
 
 import net.minecraft.init.Blocks;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+public class ExampleMod {
+
+	public static final String MODID = "examplemod";
+	public static final String VERSION = "1.0";
     
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+	@EventHandler
+	public void init(final FMLInitializationEvent event) {
 		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
-    }
+		System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+	}
 }
