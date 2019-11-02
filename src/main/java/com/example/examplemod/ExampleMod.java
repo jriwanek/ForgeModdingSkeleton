8228cd2285128962acd606a014bc729c8a56b1a5
Jasmine Iwanek <jasmine@iwanek.co.uk>
2019-11-01 19:11:05 -0700

    User a Logger, not System.out.println

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,5 +1,8 @@
 package com.example.examplemod;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -9,11 +12,13 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 public class ExampleMod {
 
 	public static final String MODID = "examplemod";
-	public static final String VERSION = "1.0";
-    
+	public static final String VERSION = "1.0.0";
+
+	public static final Logger LOGGER = LogManager.getLogger(ExampleMod.MODID);    
+
 	@EventHandler
 	public void init(final FMLInitializationEvent event) {
 		// some example code
-		System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+		LOGGER.info("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
 	}
 }
