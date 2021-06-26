371f9346ca2ae9719b76fd95aff8b172db210d68
Jasmine Iwanek <jasmine@iwanek.co.uk>
2021-06-26 13:43:47 -0400

    linting

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,5 +1,6 @@
 package com.example.examplemod;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,14 +12,30 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod {
 
+	/**
+	 *
+	 */
 	public static final String MODID = "examplemod";
+
+	/**
+	 *
+	 */
 	public static final String VERSION = "1.0.0";
 
-	public static final Logger LOGGER = LogManager.getLogger(ExampleMod.MODID);    
+	/**
+	 *
+	 */
+	public static final Logger LOGGER = LogManager.getLogger(ExampleMod.MODID);
 
+	/**
+	 *
+	 * @param event The Event.
+	 */
 	@EventHandler
 	public void init(final FMLInitializationEvent event) {
 		// some example code
-		LOGGER.info("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+		if (LOGGER.isEnabled(Level.INFO)) {
+			LOGGER.info("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+		}
 	}
 }
