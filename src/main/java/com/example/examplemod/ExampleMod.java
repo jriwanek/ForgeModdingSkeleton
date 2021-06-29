c23241db1d117a39d892586b8444451271aab95a
Jasmine Iwanek <jasmine@iwanek.co.uk>
2021-06-28 21:47:43 -0400

    Fix some PMD reports

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -19,11 +19,18 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
+/**
+ *
+ */
 @Mod("examplemod")
 public class ExampleMod {
-    // Directly reference a log4j logger.
+
+    /** Directly reference a log4j logger. */
     private static final Logger LOGGER = LogManager.getLogger();
 
+    /**
+     *
+     */
     public ExampleMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
@@ -38,17 +45,26 @@ public class ExampleMod {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    /**
+     *
+     */
     private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
+    /**
+     *
+     */
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
 
+    /**
+     *
+     */
     private void enqueueIMC(final InterModEnqueueEvent event) {
         // some example code to dispatch IMC to another mod
         InterModComms.sendTo("examplemod", "helloworld", () -> {
@@ -56,14 +72,20 @@ public class ExampleMod {
         });
     }
 
+    /**
+     *
+     */
     private void processIMC(final InterModProcessEvent event) {
         // some example code to receive and process InterModComms from other mods
         LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m -> m.getMessageSupplier().get()).
+                map(imcMap -> imcMap.getMessageSupplier().get()).
                 collect(Collectors.toList()));
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
+    /**
+     *
+     */
     @SubscribeEvent
     public void onServerStarting(final FMLServerStartingEvent event) {
         // do something when the server starts
@@ -72,9 +94,15 @@ public class ExampleMod {
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing
 	// to the MOD Event bus for receiving Registry Events)
+    /**
+     *
+     */
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
         @SubscribeEvent
+        /**
+         *
+         */
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
             // register a new block here
             LOGGER.info("HELLO from Register Block");
